3fe58dd787698bc957ecac20543135eb244e954b
HuiHola <dhruvnamdev699@gmail.com>
2024-11-17 10:20:25 +0530

    chang normal ver to loud

--- app/src/main/java/com/google/finddroid/global/CommandsGlobalVar.java
+++ app/src/main/java/com/google/finddroid/global/CommandsGlobalVar.java
@@ -11,8 +11,8 @@ public class CommandsGlobalVar {
     public static String DISABLE = "off";
     public static String NORMAL_RING= " loud";
     public static String SILENT_RING= " silent";
-    public static String DISABLE_ANTI_SWITCHOFF= "FD_FORCE_DISABLE_ANTI_SWITCHOFF";
-    public static String ENABLE_ANTI_SWITCHOFF="FD_FORCE_ENABLE_ANTI_SWITCHOFF";
+    public static String DISABLE_ANTI_SWITCHOFF= "FD_DISABLE_ANTI_SWITCHOFF";
+    public static String ENABLE_ANTI_SWITCHOFF="FD_ENABLE_ANTI_SWITCHOFF";
     public static class Description{
         public static String START_FINDDROID_COMMAND_DESCRIPTION = "Starting command it's necessary to make connection";
         public static String RING_MODE_COMMAND_DESCRIPTION = "Change Ring mode by command silent to normal or normal to silent";
